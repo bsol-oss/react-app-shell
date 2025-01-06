f062a6eff9c8fe027c25bab968b44f9346c43846
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-01-06 17:06:07 +0800

    feat: update padding

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -17,7 +17,7 @@ const Shell = ({ children, navigation, user, logo }: ShellProps) => {
   };
   return (
     <ShellContext.Provider value={shared}>
-      <Grid as="section" gridTemplateColumns={"auto 1fr"}>
+      <Grid as="section" gridTemplateColumns={"auto 1fr"} padding={'1rem'}>
         <Sidebar navigation={navigation} user={user} logo={logo} {...shared} />
         {children}
       </Grid>
