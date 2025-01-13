16479b91ff89d42c2d969c5eb0b7328eb8336241
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-01-13 11:49:10 +0800

    feat: add resize button

---
+++ src/components/Controls/ResizeButton.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@chakra-ui/react";
+import { ReactNode } from "react";
+import { RxWidth } from "react-icons/rx";
+import { widths } from "../Shell/Sidebar";
+import { useShellContext } from "../Shell/useShellContext";
+
+export interface ResizeButtonProps {
+  buttonProps?: React.RefAttributes<HTMLButtonElement>;
+  icon?: ReactNode;
+}
+
+const ResizeButton = ({
+  buttonProps = {},
+  icon = (
+    <>
+      <RxWidth />
+    </>
+  ),
+}: ResizeButtonProps) => {
+  const { sidebarWidth, setSidebarWidth } = useShellContext();
+  return (
+    <Button
+      variant="ghost"
+      justifyContent="center"
+      overflowX={"hidden"}
+      onClick={() => {
+        if (sidebarWidth > widths.min) {
+          setSidebarWidth(widths.min);
+          return;
+        }
+        if (sidebarWidth == widths.min) {
+          setSidebarWidth(widths.max);
+          return;
+        }
+        if (sidebarWidth == widths.max) {
+          setSidebarWidth(widths.min);
+          return;
+        }
+      }}
+      {...buttonProps}
+    >
+      {icon}
+    </Button>
+  );
+};
+
+export default ResizeButton;
