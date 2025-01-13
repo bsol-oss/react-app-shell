be461f7aa146259f47c13669eb5f61fc3be9c643
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-01-13 12:10:39 +0800

    chore: update shell stories

--- src/stories/Shell.stories.tsx
+++ src/stories/Shell.stories.tsx
@@ -39,59 +39,32 @@ export const PersonalBlog: Story = {
                 src={"https://picsum.photos/300/200?grayscale"}
               ></Image>
               <Flex flexFlow={"column"}>
-                <NavButton
-                  href={"/"}
-                  label="Home"
-                  icon={
-                    <>
-                      <FaHome />
-                    </>
-                  }
-                />
+                <NavButton href={"/"} label="Home" icon={<FaHome />} />
                 <NavButton
                   href={"/about"}
                   label="About"
-                  icon={
-                    <>
-                      <FaInfoCircle />
-                    </>
-                  }
+                  icon={<FaInfoCircle />}
                 />
-                <NavButton
-                  href={"/blog"}
-                  label="Blog"
-                  icon={
-                    <>
-                      <FaPenNib />
-                    </>
-                  }
-                />
+                <NavButton href={"/blog"} label="Blog" icon={<FaPenNib />} />
                 <NavButton
                   href={"/portfolio"}
                   label="Portfolio"
-                  icon={
-                    <>
-                      <FaBriefcase />
-                    </>
-                  }
+                  icon={<FaBriefcase />}
                 />
                 <NavButton
                   href={"/contact"}
                   label="Contact"
-                  icon={
-                    <>
-                      <FaEnvelope />
-                    </>
-                  }
+                  icon={<FaEnvelope />}
+                  buttonProps={{
+                    onClick: () => {
+                      console.log("good");
+                    },
+                  }}
                 />
                 <NavButton
                   href={"/services"}
                   label="Services"
-                  icon={
-                    <>
-                      <FaTools />
-                    </>
-                  }
+                  icon={<FaTools />}
                 />
               </Flex>
               <Grid
