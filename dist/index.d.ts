7dd39fccfb9ca5c93440162d62756cde1a9c464f
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-02-07 18:54:27 +0800

    fix: import

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,47 @@
-export * from '@/components/Controls/NavButton';
-export * from '@/components/Controls/UserButton';
-export * from '@/components/Controls/ResizeButton';
-export * from '@/components/Shell/Shell';
-export * from '@/components/Shell/Sidebar';
-export * from '@/components/Shell/useShellContext';
+/// <reference types="react" />
+import { ButtonProps } from '@chakra-ui/react';
+import * as react from 'react';
+import { ReactNode, Dispatch, SetStateAction } from 'react';
+
+interface NavButtonProps {
+    href: string;
+    buttonProps?: ButtonProps;
+    tag?: ReactNode;
+    icon?: ReactNode;
+    label?: string;
+}
+
+interface User {
+    name: string;
+    avatar: string;
+}
+interface UserButtonProps {
+    user: User;
+}
+
+interface ResizeButtonProps {
+    buttonProps?: React.RefAttributes<HTMLButtonElement>;
+    icon?: ReactNode;
+}
+
+interface SidebarProps {
+    navigation: ReactNode;
+    sidebarWidth: number;
+    setSidebarWidth: Dispatch<SetStateAction<number>>;
+}
+declare const widths: {
+    start: number;
+    min: number;
+    max: number;
+};
+
+interface ShellProps extends Omit<SidebarProps, "sidebarWidth" | "setSidebarWidth"> {
+    children: ReactNode;
+}
+
+declare const useShellContext: () => {
+    sidebarWidth: number;
+    setSidebarWidth: react.Dispatch<react.SetStateAction<number>>;
+};
+
+export { type NavButtonProps, type ResizeButtonProps, type ShellProps, type SidebarProps, type User, type UserButtonProps, useShellContext, widths };
