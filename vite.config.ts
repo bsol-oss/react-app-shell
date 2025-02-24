a2368d30cf89a2940c634d79d5e349522e5a0eed
jacky-ng-w3b <jacky.ng@w3bvolution.com>
2025-02-24 13:22:54 +0800

    fix: resolve atlassian esm import

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const root = resolve(__dirname, ".");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: [
+      { find: "@", replacement: resolve(root, "src") }, // Not important
+      {
+        find: "@atlaskit/pragmatic-drag-and-drop",
+        replacement: resolve(
+          root,
+          "./node_modules/@atlaskit/pragmatic-drag-and-drop/dist/esm/entry-point"
+        ),
+      },
+    ],
   },
 });
